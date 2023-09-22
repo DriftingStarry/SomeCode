9ee94885cd42de2e1b1a764ccbb96efdea36b7be
DriftingStarry <doubichenzi@126.com>
2023-09-22 17:13:41 +0800

    Update LO1.py

--- LightOffProblem/LO1.py
+++ LightOffProblem/LO1.py
@@ -17,29 +17,32 @@ lis = [[random.randint(0,1) for i in range(x)] for j in range(y)]
 lisT = [[0 for i in range(x)] for j in range(y)]
 print("目标",lisT)
 print("生成",lis)
-#打印数组lis
+
 def Prlis(lis):
+    #打印数组lis
     for i in range(len(lis)):
         tmp=''
         for j in range(len(lis[i])):
             tmp+=str(lis[i][j])
         print(tmp)
-#改变状态
+
 def change(x,y,lis):
+    #改变状态
     lis[x][y] = 1 - lis[x][y]
-    if x == 0:
-       lis[x+1][y] = 1 - lis[x+1][y]
+    
     
         
         
-#检查坐标正确度
+
 def check(x1,y1,x,y):
+    #检查坐标正确度
     if x1 >= x or y1 >= y:
         return False
     else:
         return True
-#主程序
+
 while True:
+    #主程序
     print("现在的数组为：")
     Prlis(lis)
     tmp = input("输入（坐标用,隔开。输入114514结束）：")
